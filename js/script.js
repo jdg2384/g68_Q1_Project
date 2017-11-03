60529c7e9659bddac903edaad4deefd06583c7c6
Joseph Grounds <jgrounds@Josephs-MacBook-Pro.local>
2017-11-03 09:38:31 -0600

    Post MVP Push

--- js/script.js
+++ js/script.js
@@ -1,15 +1,12 @@
 $( document ).ready(function() {
-    
     $( "#submit" ).on( "click", function() {
         event.preventDefault();
-        //$("#map").slideDown();
         $("#map").addClass("map");
-        $(".header").addClass("header");
-		
         var name = $( "#mapLookUp" ).val();
         var key = 'AIzaSyAucu-0t05lrs1nV296Sa2nkvg92qFzt7s';
         var mapKey = 'AIzaSyBj076Id5nrVYr8ULvRQ3k1F9JiYlf_-JA';
         var name = $( "#mapLookUp" ).val();
+        //Local Storage
         localStorage.setItem('Location', name);
         var $xhr = $.getJSON('https://googleplacesg68.herokuapp.com/maps/api/place/textsearch/json?query='+name+'&key='+ key);
         $xhr.done(function(data) {
@@ -20,9 +17,10 @@ $( document ).ready(function() {
                 var newHref = window.location.href+'?'+name;
                 var latitude = data.results[i].geometry.location.lat;
                 var longitude = data.results[i].geometry.location.lng;
+                console.log(latitude,longitude);
                 $("#placesHref").attr("href", "./place.html?"+name);
                 function initMap() {
-                    var uluru = {lat: latitude, lng: longitude};
+                    var uluru = {lat: latitude, lng: longitude};3
                     var map = new google.maps.Map(document.getElementById('map'), {
                       zoom: 8,
                       center: uluru,
